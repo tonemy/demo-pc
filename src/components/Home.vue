5ab3f058ee62a27a15b950eb500d8b13e32bd1cc
tonemy <1006045923@qq.com>
2020-03-05 18:19:55 +0800

    "test"

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <a-layout id="components-layout-demo-top" class="layout">
+      <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+        <navbar></navbar>
+
+      </a-layout-header>
+      <a-layout-content :style="{padding:' 0 50px', marginTop: '64px' }">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">Content</div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </div>
+
+</template>
+
+<script>
+    import Navbar from './Navbar/Navbar'
+    export default {
+       name: "home",
+       components:{
+          'navbar': Navbar
+       }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="logo" >Logo</div>
+    <a-menu
+      theme="dark"
+      v-model="current"
+      mode="horizontal"
+      :defaultSelectedKeys="['2']"
+      :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="user_home">
+        <a-icon type="home" />首页
+      </a-menu-item>
+      <a-menu-item key="app">
+        <a-icon type="shopping-cart" /> 商城
+      </a-menu-item>
+      <a-sub-menu>
+        <span slot="title" class="submenu-title-wrapper"><a-icon type="shop" />活动</span>
+        <a-menu-item-group>
+          <a-menu-item key="setting:1">活动 1</a-menu-item>
+          <a-menu-item key="setting:2">活动 2</a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+      <a-input-search
+        placeholder="输入所查询的商品"
+        style="width: 200px"
+
+      />
+
+      <a-sub-menu style="float: right">
+        <span slot="title" class="submenu-title-wrapper">    <a-icon type="user" />用户</span>
+        <a-menu-item-group>
+          <a-menu-item key="setting:5">登录</a-menu-item>
+          <a-menu-item key="setting:6">退出</a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+      <a-menu-item style="float: right">
+        <a-popover placement="bottomRight" >
+          <template slot="content">
+            <p>Content</p>
+            <p>Content</p>
+          </template>
+          <template slot="title">
+            <span>Title</span>
+          </template>
+          <a-icon type="bell"  />
+        </a-popover>
+      </a-menu-item>
+
+
+    </a-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Navbar.vue"
+    ,data () {
+      return {
+        current: ['mail'],
+      }
+    }
+    ,
+  }
+</script>
+
+<style>
+  .logo {
+    color: azure;
+    width: 30px;
+    height: 31px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px 24px 16px 0;
+    float: left;
+  }
+
+</style>
